a261ec1f6d044f24fabbe370f935774b58391f7e
Jeremy Pokorski <jeremypokorski@gmail.com>
2016-09-09 09:40:45 -0700

    remove context as a parameter from all functions and removed clearRect from move method of paddle. instead render table with a fillRect method

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,17 @@
 var tableCanvas = document.getElementById("table");
-var tableContext = tableCanvas.getContext('2d');
-tableContext.strokeStyle = "#00f";
+var context = tableCanvas.getContext('2d');
+context.strokeStyle = "#00f";
 
-function Table(context){
+function Table(){
   context.lineWidth = 4;
-  this.render = function(context) {
+  this.render = function() {
+    context.fillStyle = "white";
+    context.fillRect(1,1,274,299)
     context.strokeRect(0,0,275,300);
   }
 }
 
-function Ball(context,xpos,ypos,bsize){
+function Ball(xpos,ypos,bsize){
   this.xSpeed = Math.random() * (20-5) + 5;
   this.ySpeed = Math.random() * (20 -5) + 5;
 
@@ -17,7 +19,7 @@ function Ball(context,xpos,ypos,bsize){
   this.ypos = ypos;
   this.bsize = bsize;
 
-  this.render = function(context) {
+  this.render = function() {
     context.fillStyle = "#000"
     context.arc(this.xpos,this.ypos,this.bsize,0,2*Math.PI,false);
     context.fillStyle="black";
@@ -32,13 +34,13 @@ function Ball(context,xpos,ypos,bsize){
 // function --Paddle -- sets up a paddle with position
 //                             length and width properities
 //                             may add position properties as args later
-function Paddle(context,xpos,ypos,plength,pwidth,speed){
-  this.speed = 14;
+function Paddle(xpos,ypos,plength,pwidth,speed){
+  this.speed = 17;
   this.xpos = xpos;
   this.ypos = ypos;
   this.paddleLength = plength;
   this.paddleWidth = pwidth;
-  this.render = function(context){
+  this.render = function(){
     context.fillStyle = '#00f';
     context.fillRect(this.xpos,this.ypos,this.paddleWidth,this.paddleLength);
   };
@@ -46,7 +48,7 @@ function Paddle(context,xpos,ypos,plength,pwidth,speed){
     this.whichKey = event.key;
     // console.log(this.whichKey);
     if ((this.whichKey === 'k') && (this.ypos > 5)) {
-      context.clearRect(this.xpos,this.ypos,this.paddleWidth,this.paddleLength);
+      // context.clearRect(this.xpos,this.ypos,this.paddleWidth,this.paddleLength);
 
       this.ypos = this.ypos - this.speed;
       console.log(this.ypos);
@@ -56,7 +58,7 @@ function Paddle(context,xpos,ypos,plength,pwidth,speed){
       // if k key is pressed move paddle location up  speed number of pixels
       // unless paddle is at the top of the table
     } else if ((this.whichKey === 'm') && (this.ypos < (290 - this.paddleLength))) {
-      context.clearRect(this.xpos,this.ypos,this.paddleWidth,this.paddleLength);
+      // context.clearRect(this.xpos,this.ypos,this.paddleWidth,this.paddleLength);
 
       this.ypos = this.ypos + this.speed;
       console.log(this.ypos);
@@ -68,28 +70,29 @@ function Paddle(context,xpos,ypos,plength,pwidth,speed){
     }
   }
 
+
+
+
+var ntable = new Table();
+var nball = new Ball(125,145,6);
+var computerPaddle = new Paddle(15,15,55,12);
+var playPaddle = new Paddle(250,245,55,12);
+
+// function render(context) takes a context object and calls the render functions
+//                           of the two paddles and ball objects
+var render = function() {
+  ntable.render();
+  nball.render();
+  playPaddle.render();
+  computerPaddle.render();
   window.onkeypress = function(e){
     playPaddle.move(e);
   }
 
-
-var ntable = new Table(tableContext);
-var nball = new Ball(tableContext,125,145,6);
-var computerPaddle = new Paddle(tableContext,15,15,55,12);
-var playPaddle = new Paddle(tableContext,250,245,55,12);
-
-// function render(context) takes a context object and calls the render functions
-//                           of the two paddles and ball objects
-var render = function(context) {
-  nball.render(tableContext);
-  playPaddle.render(tableContext);
-  computerPaddle.render(tableContext);
-  // ntable.render(tableContext);
-
 }
 
 var Step = function () {
-  render(tableContext);
+  render();
   animate(Step);
 
 
@@ -97,7 +100,7 @@ var Step = function () {
 
 window.onload = function() {
   // ntable.render(tableContext);
-  render(tableContext);
+  // render(tableContext);
   console.log('beforeanimate');
   animate(Step);
   console.log('afteranimate');
@@ -109,4 +112,4 @@ var animate = window.requestAnimationFrame ||
         window.mozRequestAnimationFrame    ||
         window.oRequestAnimationFrame     ||
         window.msRequestAnimationFrame     ||
-        function(Step) { window.setTimeout(Step, 1000/60) };
+        function(Step) { window.setTimeout(Step, 1000/100) };
